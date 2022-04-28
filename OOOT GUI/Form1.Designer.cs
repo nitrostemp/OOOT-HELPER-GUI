9083e81118e071659b643a2a62c91994e662b9da
ajanhallinta <4janhallinta@gmail.com>
2022-04-29 02:26:11 +0300

    typo

--- OOOT GUI/Form1.Designer.cs
+++ OOOT GUI/Form1.Designer.cs
@@ -106,7 +106,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(289, 23);
             this.button6.TabIndex = 5;
-            this.button6.Text = "Copy ROMs";
+            this.button6.Text = "Copy ROM";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
